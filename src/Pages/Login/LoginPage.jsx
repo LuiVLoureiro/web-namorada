43358540c4c8ad639a5a612a3455a5eb26393954
Dougladmo <douglas.costa7417@gmail.com>
2024-06-12 07:22:42 -0300

    login simulado adicionado e link to model-register e client-register

--- src/Pages/Login/LoginPage.jsx
+++ src/Pages/Login/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 import './LoginPage.css'
 
@@ -6,16 +7,26 @@ import google from '../../images/google.svg'
 import facebook from '../../images/facebook.svg'
 
 const LoginPage = () => {
+  const navigate = useNavigate()
+
+  const navigateToExplore = () => {
+    navigate('/explore/');
+  }
+
   return (
     <div className='main-content'> 
       <div className='login_wrapper'>
         <h1>Login</h1>
         <p>Faça o login para entrar no melhor site de webnamoro da internet</p>
-        <form>
+        <form onSubmit={navigateToExplore}>
           <input type="email" name="email" required id="email" placeholder='Digite seu email' />
           <input type="password" name="password" required id="password" placeholder='Digite sua senha' />
           <input type="submit" value="Entrar" />
         </form>
+        <div className='new-users'>
+          <a href="/client-register/">Novo por aqui? Cadastre-se como usuário</a>
+          <a href="/model-register/">Nova por aqui? Cadastre-se como modelo</a>
+        </div>
         <div className='login_ways'>
           <hr />
           <p>Outras formas de Login</p>
